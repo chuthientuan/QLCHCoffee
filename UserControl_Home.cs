bf0a667218a75646b463e48f6828e655c3dd783c
Loz <tuanchu24112004@gmail.com>
2024-10-03 15:34:32 +0700

    Test user control

---
+++ UserControl_Home.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL
+{
+    public partial class UserControl_Home : UserControl
+    {
+        public event EventHandler OnSwitchToLogin;
+        public UserControl_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (OnSwitchToLogin != null)
+            {
+                OnSwitchToLogin(this, EventArgs.Empty);
+            }
+        }
+    }
+}
